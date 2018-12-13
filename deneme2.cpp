050b19839534868b5a665c321b8b862bf2cf41e1
ArthurJacopson <arifyakoup@yahoo.com>
2018-12-13 13:26:29 +0200

    Switch ilk kullanim

---
+++ deneme2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int yas;
+	cout << "Lutfen yasinizi giriniz" << endl;
+	cin >> yas;
+
+	switch (yas)
+	{
+		case 15;
+		cout << "sa knk" << endl;
+		break;
+
+		case yas < 15;
+		cout << "buyu de gel CICEK" << endl;
+
+		case yas > 15;
+		cout << "Ozur dilerim abe" << endl;
+	}
+	int x;
+	cin >> x;
+}
